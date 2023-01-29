86b1179a1a3e01fa881f9792121dd7649ba4b593
Jie Zhu <zhuje@amazon.com>
2023-01-28 23:50:33 -0800

    [plugins] add lsp relevants plugins

---
+++ lua/mynvim/plugins/lsp/init.lua
@@ -0,0 +1,198 @@
+-- mainly inspired by LazyVim LSP
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/lsp/init.lua
+
+local function on_attach(handler)
+    vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+            local buffer = args.buf
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            handler(client, buffer)
+        end,
+  })
+end
+
+return {
+    -- cmdline tools and lsp servers
+    {
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        -- keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+
+        opts = {
+            ensure_installed = require("mynvim.plugins.lsp.install").mason,
+        },
+        ---@param opts MasonSettings | {ensure_installed: string[]}
+        config = function(_, opts)
+            require("mason").setup(opts)
+            local mr = require("mason-registry")
+            for _, tool in ipairs(opts.ensure_installed) do
+                local p = mr.get_package(tool)
+                if not p:is_installed() then
+                    p:install()
+                end
+            end
+        end,
+    },
+
+    -- Use Neovim as a language server to inject LSP features with arbitrary
+    -- programs
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        event = "BufReadPre",
+        dependencies = { "mason.nvim" },
+        opts = function()
+            local nls = require("null-ls")
+            return {
+                -- check the full list:
+                --   https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+                sources = {
+                    -- nls.builtins.formatting.prettierd,
+                    nls.builtins.formatting.stylua,
+                },
+            }
+        end,
+    },
+
+    -- auto completion
+    {
+        "hrsh7th/nvim-cmp",
+        version = false, -- last release is way too old
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+        },
+        opts = function()
+            local cmp = require("cmp")
+            return {
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-Space>"] = cmp.mapping.complete({}),
+                    ["<Tab>"] = function()
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        else
+                            cmp.complete()
+                        end
+                    end,
+                    ["<C-e>"] = cmp.mapping.abort(),
+
+                    -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                formatting = {
+                    format = function(_, item)
+                        local icons = require("mynvim.configs.icons").icons.kinds
+                        if icons[item.kind] then
+                            item.kind = icons[item.kind] .. item.kind
+                        end
+                        return item
+                    end,
+                },
+                experimental = {
+                    ghost_text = {
+                        hl_group = "LspCodeLens",
+                    },
+                    native_menu = false,
+                },
+            }
+        end,
+    },
+
+    -- lspconfig
+    {
+        "neovim/nvim-lspconfig",
+        event = "BufReadPre",
+        dependencies = {
+            { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+            { "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
+            "mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        ---@class PluginLspOpts
+        opts = {
+            -- options for vim.diagnostic.config()
+            diagnostics = {
+                underline = true,
+                update_in_insert = false,
+                virtual_text = { spacing = 4, prefix = "●" },
+                severity_sort = true,
+            },
+
+            -- options for vim.lsp.buf.format
+            format = {
+                formatting_options = nil,
+                timeout_ms = nil,
+            },
+
+            -- LSP Server Settings
+            servers = require("mynvim.plugins.lsp.install").servers,
+
+            setup = require("mynvim.plugins.lsp.install").setup,
+        },
+        ---@param opts PluginLspOpts
+        config = function(_, opts)
+            -- setup formatting and keymaps
+            on_attach(function(client, buffer)
+                require("mynvim.plugins.lsp.keymaps").on_attach(client, buffer)
+            end)
+
+            -- diagnostics
+            for name, icon in pairs(require("mynvim.configs.icons").icons.diagnostics) do
+                name = "DiagnosticSign" .. name
+                vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+            end
+            vim.diagnostic.config(opts.diagnostics)
+
+            local servers = opts.servers
+            local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+            local function setup(server)
+                local server_opts = servers[server] or {}
+                server_opts.capabilities = capabilities
+                if opts.setup[server] then
+                    if opts.setup[server](server, server_opts) then
+                        return
+                    end
+                elseif opts.setup["*"] then
+                    if opts.setup["*"](server, server_opts) then
+                        return
+                    end
+                end
+                require("lspconfig")[server].setup(server_opts)
+            end
+
+            local mlsp = require("mason-lspconfig")
+            local available = mlsp.get_available_servers()
+
+            local ensure_installed = {} ---@type string[]
+            for server, server_opts in pairs(servers) do
+                if server_opts then
+                    server_opts = server_opts == true and {} or server_opts
+                    -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+                    if server_opts.mason == false or not vim.tbl_contains(available, server) then
+                        setup(server)
+                    else
+                        ensure_installed[#ensure_installed + 1] = server
+                    end
+                end
+            end
+
+            require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
+            require("mason-lspconfig").setup_handlers({ setup })
+        end,
+    },
+}
