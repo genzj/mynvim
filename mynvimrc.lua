c235ba33313bbc529aff082b36f2033d203b3a2f
Jie Zhu <zhuje@amazon.com>
2023-01-28 21:47:15 -0800

    [chores] disable plugin update notice & change detection

--- mynvimrc.lua
+++ mynvimrc.lua
@@ -2,36 +2,43 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  spec = {
-    -- import LazyVim plugins
-    { import = "mynvim.plugins" },
-    -- import/override with your plugins
-    -- { import = "plugins" },
-  },
-  defaults = {
-      lazy = true,
-  },
-  performance = {
-      rtp = {
-		-- do not reset rtp on win32 as I want to clone
-		-- the configs to a different path instead of
-		-- the default AppData config dir
-        reset = (vim.fn.has('win32') == 0),
-	  }
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+    spec = {
+        -- import LazyVim plugins
+        { import = "mynvim.plugins" },
+        -- import/override with your plugins
+        -- { import = "plugins" },
+    },
+    defaults = {
+        lazy = true,
+    },
+    performance = {
+        rtp = {
+            -- do not reset rtp on win32 as I want to clone
+            -- the configs to a different path instead of
+            -- the default AppData config dir
+            reset = (vim.fn.has('win32') == 0),
+        }
+    },
+    install = { colorscheme = { "tokyonight", "habamax" } },
+    checker = {
+        -- automatically check for plugin updates
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        enabled = false,
+    },
 })
