8f5434320309172fc5231dce41490956a0190cc3
Jie Zhu <zhuje@amazon.com>
2023-02-01 13:49:26 -0800

    [plugins] add toggleterm

---
+++ lua/mynvim/plugins/terminal.lua
@@ -0,0 +1,28 @@
+return {
+    "akinsho/toggleterm.nvim",
+    keys = {
+        [[<c-`>]],
+        { "<leader>yt", "<cmd>ToggleTermSendCurrentLine<cr>", mode = "n", desc = "sends the whole line where you are standing with your cursor to the terminal" },
+        { "<leader>yt", "<cmd>ToggleTermSendVisualSelection<cr>", mode = "v", desc = "sends the visually selected text to the terminal" },
+    },
+    opts = {
+        open_mapping = [[<c-`>]],
+    },
+    config = function (_, opts)
+        require('toggleterm').setup(opts)
+        vim.api.nvim_create_autocmd(
+            "TermOpen",
+            {
+                group = vim.api.nvim_create_augroup("MyNVimTerm", {
+                    clear = true,
+                }),
+
+                pattern = "term://*",
+                callback = function ()
+                    local kopts = {buffer = 0}
+                    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], kopts)
+                end,
+            }
+        )
+    end
+}
