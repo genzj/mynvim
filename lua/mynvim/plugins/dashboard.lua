b0c1f484442bfe84d9b9c7ee15986f3832d34dce
Jie Zhu <zhuje@amazon.com>
2023-03-29 17:48:47 -0700

    [dashboard] add recent file browser

--- lua/mynvim/plugins/dashboard.lua
+++ lua/mynvim/plugins/dashboard.lua
@@ -18,6 +18,7 @@ return {
             dashboard.section.buttons.val = {
                 -- dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
                 dashboard.button("n", "	" .. "New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("r", " 	" .. "Recent files", ":browse oldfiles <CR>"),
                 -- dashboard.button("r", " " .. "Recent files", ":Telescope oldfiles <CR>"),
                 -- dashboard.button("g", " " .. "Find text", ":Telescope live_grep <CR>"),
                 dashboard.button("c", "	" .. "Config", ":call chdir(fnamemodify($MYVIMRC, ':p:h')) <BAR> :e $MYVIMRC <CR>"),
