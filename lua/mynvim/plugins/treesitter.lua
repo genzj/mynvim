9a7a86afd53f808fe53a9ec1ce84fc1d12668def
genzj <zj0512@gmail.com>
2025-04-14 16:32:25 -0700

    feat(keymap): :sparkles: use ctrl+/ to init/increase block selection as ctrl+space doesn't work on some platforms

--- lua/mynvim/plugins/treesitter.lua
+++ lua/mynvim/plugins/treesitter.lua
@@ -1,28 +1,34 @@
+---@module 'lazy.types'
+---@type LazySpec[]
 return {
     {
         -- rainbow-delimiters isn't a TS plugin but must work with TS.
         -- will be loaded from TS setup
         'HiPhish/rainbow-delimiters.nvim',
-        url = 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git',
+        lazy = true,
     },
     {
         "nvim-treesitter/nvim-treesitter",
         version = false, -- last release is way too old and doesn't work on Windows
         build = ":TSUpdate",
         event = "BufReadPost",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
         keys = {
-            { "<c-space>", desc = "Increment selection" },
+            { "<c-/>", desc = "Init/Increment selection" },
             { "<bs>", desc = "Schrink selection", mode = "x" },
         },
         ---@type TSConfig
+        ---@diagnostic disable-next-line
         opts = {
-            indent = { enable = true },
             ensure_installed = require("mynvim.configs").install.treesitter,
+            indent = { enable = true },
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
+                    init_selection = "<C-/>",
+                    node_incremental = "<C-/>",
                     scope_incremental = "<nop>",
                     node_decremental = "<bs>",
                 },
@@ -32,7 +38,7 @@ return {
                 -- disable slow treesitter highlight for large files
                 disable = function(_, buf)
                     local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
                     if ok and stats and stats.size > max_filesize then
                         return true
                     end
@@ -41,12 +47,12 @@ return {
             },
         },
         ---@param opts TSConfig
-        config = function(plugin, opts)
+        config = function(_, opts)
             require("nvim-treesitter.configs").setup(opts)
             vim.opt.foldmethod = "expr"
             vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
             vim.opt.foldenable = false
-            require("rainbow-delimiters").enable()
+            require("rainbow-delimiters").enable(0)
         end,
     },
 }
